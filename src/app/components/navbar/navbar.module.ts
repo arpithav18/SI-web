27e23c8ca5e1ee281b7cd1e1175b24c7efa017fd
arpithav <arpithav18@gmail.com>
2019-01-05 19:42:16 +0530

    initial commit

---
+++ src/app/components/navbar/navbar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+   
+  ],
+  declarations: [NavbarComponent]
+})
+export class NavbarModule { }
