19106a0ede4c2eeda27404ac9bdf1eab81769a83
arpithav <arpithav18@gmail.com>
2019-01-05 20:58:30 +0530

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-// import { NavbarComponent } from './components/navbar/navbar/navbar.component';
-import {NavbarModule} from './components/navbar/navbar.module';
+import {HomeModule} from '../app/components/home.module';
 @NgModule({
   declarations: [
     AppComponent,
-    // NavbarComponent
+   
   ],
   imports: [
     BrowserModule,
-    NavbarModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home.module.ts
+++ src/app/components/home.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { NavbarComponent } from './navbar/navbar.component';
-
+import { NavbarComponent } from '../components/navbar/navbar/navbar.component';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
-   
+    MatButtonModule
   ],
+  exports:[NavbarComponent],
   declarations: [NavbarComponent]
 })
-export class NavbarModule { }
+export class HomeModule { }
