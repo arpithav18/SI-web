4403f6abcd9a3f47108d7ad0509e622f408d1f9a
arpithav <arpithav18@gmail.com>
2019-01-10 20:41:31 +0530

    bootstrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HomeComponent} from '../app/components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
+import { ScreenshotsComponent } from './components/screenshots/screenshots.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
-   HomeComponent
+   HomeComponent,
+   NavbarComponent,
+   AboutUsComponent,
+   TechnologiesComponent,
+   ScreenshotsComponent,
+   BlogComponent,
+   ContactComponent
   ],
   imports: [
     BrowserModule,
